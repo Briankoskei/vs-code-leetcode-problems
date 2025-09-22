d68107d4c64c3752af1081752070bd7ddc5731fe
Briankoskei <koskeybrian873@gmail.com>
2025-09-22 08:13:29 +0300

    PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,6 +1,6 @@
 public class PalindromeNumber {
-    public boolean isPalindrome(int x) {
-        // Negative numbers and numbers ending in 0 (but not 0 itself) are not palindromes
+    public boolean isPalindrome(int x) 
+    {
         if (x < 0 || (x % 10 == 0 && x != 0)) return false;
 
         int reversed = 0;
@@ -8,9 +8,6 @@ public class PalindromeNumber {
             reversed = reversed * 10 + x % 10;
             x /= 10;
         }
-
-        // For even-length numbers: x == reversed
-        // For odd-length numbers: x == reversed / 10
         return x == reversed || x == reversed / 10;
     }
 }
